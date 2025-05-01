693938c869caf8b8a372e93ab0b9c4ff40f79647
Yasmin Lins <yasminlins.sites@gmail.com>
2025-05-01 18:51:29 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace budget.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
